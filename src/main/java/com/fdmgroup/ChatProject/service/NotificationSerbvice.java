c3f27231705523aab5980b65205e2da63602ccf5
FilipDabrowski <fil.dabr@gmail.com>
2023-02-06 18:35:29 +0100

    Create NotificationSerbvice.java

---
+++ src/main/java/com/fdmgroup/ChatProject/service/NotificationSerbvice.java
@@ -0,0 +1,7 @@
+package com.fdmgroup.ChatProject.service;
+
+import com.fdmgroup.ChatProject.service.interfaces.INotificationService;
+
+public class NotificationSerbvice implements INotificationService {
+
+}
