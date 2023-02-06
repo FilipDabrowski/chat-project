2713da3e3e7a46379cd052152c5fa967e07e0059
FilipDabrowski <fil.dabr@gmail.com>
2023-02-06 18:35:45 +0100

    Create UniqueUserRepository.java

---
+++ src/main/java/com/fdmgroup/ChatProject/repository/UniqueUserRepository.java
@@ -0,0 +1,18 @@
+package com.fdmgroup.ChatProject.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fdmgroup.ChatProject.model.UniqueUser;
+
+
+public interface UniqueUserRepository extends JpaRepository<UniqueUser, Long> {
+
+	Optional<UniqueUser> findByEmail(String email);
+
+	Optional<UniqueUser> findByName(String name);
+
+
+
+}
