69fb116be47befb28e8198e1914b898f87d75af1
FilipDabrowski <fil.dabr@gmail.com>
2023-02-06 18:35:27 +0100

    Create UniqueUserService.java

---
+++ src/main/java/com/fdmgroup/ChatProject/service/UniqueUserService.java
@@ -0,0 +1,7 @@
+package com.fdmgroup.ChatProject.service;
+
+import com.fdmgroup.ChatProject.service.interfaces.IUniqueUserService;
+
+public class UniqueUserService implements IUniqueUserService {
+
+}
