a9014cbdc43df66d86654881f45d2b1ba9202d1b
FilipDabrowski <fil.dabr@gmail.com>
2023-02-07 11:21:25 +0100

    Update Chat.java

--- src/main/java/com/fdmgroup/ChatProject/model/Chat.java
+++ src/main/java/com/fdmgroup/ChatProject/model/Chat.java
@@ -10,12 +10,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 
 
 @Entity
+@Table(name="chat")
 public class Chat {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
