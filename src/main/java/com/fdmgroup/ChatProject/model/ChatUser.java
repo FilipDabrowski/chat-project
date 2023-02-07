1571c825b40fd5f5d419471487acc5c4bdb283aa
FilipDabrowski <fil.dabr@gmail.com>
2023-02-07 09:41:34 +0100

    Update ChatUser.java

--- src/main/java/com/fdmgroup/ChatProject/model/ChatUser.java
+++ src/main/java/com/fdmgroup/ChatProject/model/ChatUser.java
@@ -1,5 +1,13 @@
 package com.fdmgroup.ChatProject.model;
 
-public class ChatUser {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class ChatUser {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id; 
 }
