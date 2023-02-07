2e04322b4a66358b33932267d3f4cb7130557848
FilipDabrowski <fil.dabr@gmail.com>
2023-02-07 10:30:59 +0100

    Update NotificationRepository.java

--- src/main/java/com/fdmgroup/ChatProject/repository/NotificationRepository.java
+++ src/main/java/com/fdmgroup/ChatProject/repository/NotificationRepository.java
@@ -1,9 +1,10 @@
 package com.fdmgroup.ChatProject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fdmgroup.ChatProject.model.Notification;
-
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
 }
