cfadef247b47ef5fe27c0349bda59af31ffd4596
FilipDabrowski <fil.dabr@gmail.com>
2023-02-07 10:31:05 +0100

    Update UniqueUserRepository.java

--- src/main/java/com/fdmgroup/ChatProject/repository/UniqueUserRepository.java
+++ src/main/java/com/fdmgroup/ChatProject/repository/UniqueUserRepository.java
@@ -3,13 +3,14 @@ package com.fdmgroup.ChatProject.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fdmgroup.ChatProject.model.UniqueUser;
 
-
+@Repository
 public interface UniqueUserRepository extends JpaRepository<UniqueUser, Long> {
 
-	Optional<UniqueUser> findByEmail(String emailAdress);
+	Optional<UniqueUser> findByEmailAdress(String emailAdress);
 
 	Optional<UniqueUser> findByName(String name);
 
